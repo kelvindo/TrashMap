bf9eb2bea9eb0f76cb2fab98a488bb9ed28fefc6
Jessica Kung <jzkung@gmail.com>
2012-11-06 19:02:30 -0800

    Fixed bg image. Oh, also made separate .php files for rules, found.

--- menu.php
+++ menu.php
@@ -15,29 +15,13 @@
 		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 		<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
 
-		<style type="text/css">
-			body {
-				background: url('http://i.imgur.com/zNTdD.png');
-			}
-			.ui-page {
-				background: transparent;
-			}
-			
-			.ui-content {
-				background: transparent;
-			}
-		</style>
 	</head>
 	<body>
-		<script>
-			alert('<?php echo $user ?>');
-		</script>
-	
 		<div data-role="page" id="menu" data-theme="c">
 			<div data-role="header">
 				<h1>Menu</h1>
 			</div>
-			<div data-role="content">		
+			<div data-role="content">
 				<a data-role="button" onClick="$.mobile.changePage( 'game-mode.php', { transition: 'pop' } );">Game Mode</a>
 				<a data-role="button" onClick="$.mobile.changePage( 'quick-find.php', { transition: 'pop' } );">Quick Find</a>
 				<a data-role="button" onClick="$.mobile.changePage( 'account.php', { transition: 'pop' } );">My Account</a>
