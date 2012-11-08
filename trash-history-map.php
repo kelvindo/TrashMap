182293220c9f6a33802d0ef80e51271b56177b69
Kevin <klu1@stanford.edu>
2012-11-08 14:48:17 -0800

    changed trash history map with working points

--- trash-history-map.php
+++ trash-history-map.php
@@ -7,7 +7,53 @@
 		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
 		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 		<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+		<script src="http://www.google.com/jsapi?key=AIzaSyDUGIMFHD0MoPLaGQFQUwWBFwPr3zTVWIg&autoload=%7Bmodules%3A%5B%7Bname%3A%22maps%22%2Cversion%3A3%2Cother_params%3A%22sensor%3Dtrue%22%7D%5D%7D"></script>
 
+		<?php
+		include ("config.php");
+		$query = "SELECT * FROM trashcans";
+		$result = mysql_query($query);
+		
+		echo "<script type='text/javascript'>\n";
+		echo "var trashcans = new Array();\n";
+		
+		while($row = mysql_fetch_array($result)) {
+			echo "trashcans[trashcans.length] = { x: '". $row['x'] ."', y: '". $row['y'] ."', id: '". $row['T_Id'] ."'};\n";
+		}
+		echo "</script>";
+		?>
+
+		<script type="text/javascript">
+		google.setOnLoadCallback(function() {
+			var myOptions = {
+				zoom: 4,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+			var map = new google.maps.Map(document.getElementById("map"), myOptions);
+			if (typeof(navigator.geolocation) != 'undefined') {
+				navigator.geolocation.getCurrentPosition(function(position) {
+					firstPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+					map.setCenter(firstPosition);
+				});
+			}
+			waitForTrashCans();
+
+			function waitForTrashCans() {
+				if (trashcans) {
+					for (var i = 0; i < trashcans.length; i++) {
+						var trashPoint = new google.maps.LatLng(trashcans[i].x, trashcans[i].y);
+						var trashMarker = new google.maps.Marker({
+							position: trashPoint,
+							map: map
+						});
+					}
+				} else {
+					setTimeout(waitForTrashCans, 1000);
+				}
+			}
+		});
+
+		</script>
 	</head>
 	<body>
 		<div data-role="page" id="trash-history-map" data-theme="c">
@@ -16,7 +62,7 @@
 				<a data-role="button" href="account.php" data-icon="back" data-iconpos="notext"></a>
 			</div>
 			<div data-role="content">		
-				<img src="http://i.imgur.com/xUfwU.gif" alt="Your Trash History" width=100% height=90%>
+				<div id="map" style="position:absolute;left:0px;top:42px;right:0px;bottom:35px;"></div>
 			</div>
 			<div data-role="footer" data-position="fixed" data-id="trash-history-footer">
 				<div data-role="navbar">
